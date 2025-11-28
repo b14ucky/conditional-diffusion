efcd896b580b55902aa68dc136e628e363f13a85
Dominik <101473446+b14ucky@users.noreply.github.com>
2025-11-28 11:32:56 +0100

    feat: add chat UI

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+prompt = st.chat_input(placeholder="How can I help you today?")
+
+if prompt:
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    )
+
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "content": f"You said: {prompt}",
+        }
+    )
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
