7b3d80901ccdc4016d45825a8807626b11aaecf4
Dominik <101473446+b14ucky@users.noreply.github.com>
2025-12-12 03:02:53 +0100

    feat: implement CIFAR-10 image generation and update chat response handling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 load_dotenv()
 
@@ -29,11 +30,50 @@ def get_contents_from_history(history: list[dict]) -> list[types.Content]:
         if role == "assistant":
             role = "model"
         content = types.Content(
-            role=role, parts=[types.Part.from_text(text=message["content"])]
+            role=role,
+            parts=[types.Part.from_text(text=message["content"])],
         )
         contents.append(content)
 
     return contents
+
+
+def generate_cifar_image(label: str) -> Figure:
+    fig = plt.figure()
+    plt.title(label)
+    plt.bar(np.arange(10), np.random.rand(10))
+    return fig
+
+
+tool = types.Tool(
+    function_declarations=[
+        types.FunctionDeclaration(
+            name="generate_cifar_image",
+            description="Generate CIFAR-10 image",
+            parameters_json_schema={
+                "type": "object",
+                "properties": {
+                    "label": {
+                        "type": "string",
+                        "description": "One of the 10 valid CIFAR-10 classes",
+                    },
+                },
+                "required": ["label"],
+            },
+        )
+    ]
+)
+
+
+config = types.GenerateContentConfig(
+    system_instruction="""You are a virtual assistant that helps the user with image generation.
+    If user asks you to generate an image you invoke the diffusion model trained on CIFAR-10 dataset,
+    only if the user wants to generate an image from the classes available in the dataset.
+    Only then you call the function with appropriate class name.
+    Otherwise you should tell the user, that it is not possible to generate the desired image.
+    Make sure to always respond with some kind of a nice reply, like "Here's your image of a cat" """,
+    tools=[tool],
+)
 
 
 if "history" not in st.session_state:
@@ -57,20 +97,29 @@ if prompt:
     chat = client.chats.create(
         model="gemini-2.5-flash-lite",
         history=get_contents_from_history(st.session_state.history),  # type: ignore
+        config=config,
     )
 
-    with st.chat_message("assistant"):
-        response = st.write_stream(
-            chunk.text for chunk in chat.send_message_stream(prompt)
-        )
+    fig_container = [None]
 
-    # fig = plt.figure()
-    # plt.bar(np.arange(10), np.random.rand(10))
+    def stream_generator():
+
+        for chunk in chat.send_message_stream(prompt):
+            if hasattr(chunk, "function_calls"):
+                if chunk.function_calls:
+                    fig_container[0] = generate_cifar_image(**chunk.function_calls[0].args)  # type: ignore
+
+            yield chunk.text if chunk.text else ""
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream_generator())
+        if fig_container[0]:
+            st.pyplot(fig_container[0])
 
     st.session_state.history.append(
         {
             "role": "assistant",
             "content": response,
-            # "figure": fig,
+            "figure": fig_container[0],
         }
     )
